b8a5d72c2ff1fac95234c6d61810549cf619fa36
Rima Amal Maji <2025.rimam@isu.ac.in>
2026-09-19 11:51:27 +0530

    Add StudentMarks class for managing student data

---
+++ marks/StudentMarks.java
@@ -0,0 +1,38 @@
+package marks;
+
+public class StudentMarks {
+
+    private int rollNo;
+    private String name;
+    private double marks;
+
+    public StudentMarks(int rollNo, String name, double marks) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+}
